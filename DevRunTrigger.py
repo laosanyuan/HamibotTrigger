a47cf135702b143d89112202a0168ed022d4bef8
一刀 <yhonglai@163.com>
2023-08-20 22:56:53 +0800

    feat 添加执行脚本

---
+++ DevRunTrigger.py
@@ -0,0 +1,26 @@
+import sys
+import requests
+
+
+def run(token, script, robots):
+    url = f"https://api.hamibot.com/v1/devscripts/{script}/run"
+    headers = {
+        "Authorization": token,
+        "Content-Type": "application/json"
+    }
+    data = {
+        robots
+    }
+    response = requests.post(url, headers=headers, json=data)
+
+    print("HTTP 状态码:", response.status_code)
+    print("响应内容:", response.text)
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]  # 排除脚本名称
+    token = args[0]
+    script = args[1]
+    robots = args[2]
+
+    run(token, script, robots)
