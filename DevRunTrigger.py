89d7b21bec47cd9797ac5ee436f3c93400cfd7ee
一刀 <yhonglai@163.com>
2023-08-21 23:41:19 +0800

    ci 更行流程

--- DevRunTrigger.py
+++ DevRunTrigger.py
@@ -1,15 +1,19 @@
 import sys
 import requests
+import json
 
 
 def run(token, script, robots):
+    print(f"script:{script}")
+    print(f"robots:{robots}")
+
     url = f"https://api.hamibot.com/v1/devscripts/{script}/run"
     headers = {
         "Authorization": token,
         "Content-Type": "application/json"
     }
     data = {
-        robots
+        "robots" : robots
     }
     response = requests.post(url, headers=headers, json=data)
 
@@ -19,8 +23,20 @@ def run(token, script, robots):
 
 if __name__ == "__main__":
     args = sys.argv[1:]  # 排除脚本名称
-    token = args[0]
-    script = args[1]
-    robots = args[2]
+    token =  args[0]
+    paras = args[1]
+    name = args[2]
 
-    run(token, script, robots)
+    try:
+        data = json.loads(paras)
+    except json.JSONDecodeError:
+        print("解析参数失败，可能是Hamibot参数格式设置错误")
+        sys.exit(1)
+
+    matching_key = next((key for key in data if key.upper() == name), None)
+
+    if matching_key is not None:
+        value = data[matching_key]
+        run(token, value["script"], value["robots"])
+    else:
+        print(f"没有获取到 '{name}' 对应的参数")
